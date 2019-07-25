754f5ae2f59d85bb710253beabf226b0add399e0
vanlongvl99 <nguyenvanlongt2@gmail.com>
2019-07-25 15:09:46 +0700

    aa.py

--- aa.py
+++ aa.py
@@ -3,6 +3,10 @@ import os
 import time
 import datetime
 import copy
+import readchar
+import threading
+import queue
+
 
 screen=[ [" "," "," "," "," "," "," "," "," "," "],
          [" "," "," "," "," "," "," "," "," "," "],
@@ -25,33 +29,33 @@ screen=[ [" "," "," "," "," "," "," "," "," "," "],
          [" "," "," "," "," "," "," "," "," "," "],
          [" "," "," "," "," "," "," "," "," "," "]]
 
-block_0=[["$","$"],
-         ["$","$"]]
+block_0=[["*","*"],
+         ["*","*"]]
           
-block_1=[["$","$","$"],
-         [" ","$"," "],
+block_1=[["*","*","*"],
+         [" ","*"," "],
          [" "," "," "]]
         
-block_2=[[" ","$","$"],
-         ["$","$"," "],
+block_2=[[" ","*","*"],
+         ["*","*"," "],
          [" "," "," "]]
 
-block_3=[["$"," "," "],
-         ["$","$","$"],
+block_3=[["*"," "," "],
+         ["*","*","*"],
          [" "," "," "]]
 
 block_4=[[" "," "," "," "],
-         ["$","$","$","$"],
+         ["*","*","*","*"],
          [" "," "," "," "],
          [" "," "," "," "],]
 
 
-block_5=[[" "," ","$"],
-         ["$","$","$"],
+block_5=[[" "," ","*"],
+         ["*","*","*"],
          [" "," "," "]]
 
-block_6=[["$","$"," "],
-         [" ","$","$"],
+block_6=[["*","*"," "],
+         [" ","*","*"],
          [" "," "," "]]
 
 
@@ -77,31 +81,32 @@ def merge_block_with_screen(screen_phu,block_x,range_of_block):
     for row in range(len(block_x)):
         block_row=block_x[row]
         for colum in range(len(block_row)):
-            if block_row[colum]=="$":
-                block_in_screen[row+range_of_block[0]][colum+range_of_block[1]]="$"
+            if block_row[colum]=="*":
+                block_in_screen[row+range_of_block[0]][colum+range_of_block[1]]="*"
     return block_in_screen
 
 #merge screen and display
 #input: screen [][]
 #kiểm tra rồi
 def display_screen(screen_,score):
+    time.sleep(0.5)
     os.system("clear")
     merge_scr=[]
-    print("######")
-    print("#",score[0],"#")
-    print("######")
-    print("======================\n")
+    thread_println("######")
+    thread_println("#"+str(score[0])+"#")
+    thread_println("######")
+    thread_println("======================\n")
     for row in range(len(screen_)):
         string_row =""
         for colum in range(len(screen_[row])):
             string_row+=screen_[row][colum]
         merge_scr.append(string_row) 
     for row in range(len(merge_scr)):
-        print(merge_scr[row])
+        thread_println(merge_scr[row])
 
 #dich trai:
 #input: screen and block, range of block
-#output: new_range
+#output: new_screen
 #kiểm tra rồi
 def move_left(screen_phu,block_x,range_of_block):
     new_screen=copy.deepcopy(screen_phu)
@@ -109,14 +114,14 @@ def move_left(screen_phu,block_x,range_of_block):
     range_new=copy.deepcopy(range_of_block)
     linh_canh=1
     for row in range(len(new_screen)):
-        if new_screen[row][0]=="$":
+        if new_screen[row][0]=="*":
             return range_new
     range_new[1]=range_new[1]-1
     return range_new
     
 #dich phải:
 #input: screen and block, range of block
-#output: new_range
+#output: new_screen
 #kiểm tra rồi
 def move_right(screen_phu,block_x,range_of_block):
     new_screen=copy.deepcopy(screen_phu)
@@ -124,14 +129,14 @@ def move_right(screen_phu,block_x,range_of_block):
     range_new=copy.deepcopy(range_of_block)
     linh_canh=1
     for row in range(len(new_screen)):
-        if new_screen[row][9]=="$":
+        if new_screen[row][9]=="*":
             return range_new
     range_new[1]+=1
     return range_new
 
 # xuống hàng:
 #input: screen and block, range of block
-#output: new_range
+#output: new_screen
 #kiểm tra rồi,chưa khắc phục được vài lỗi
 def down_1_line(screen_phu,block,range_of_block):
     range_new=copy.deepcopy(range_of_block)
@@ -162,13 +167,13 @@ def xoay_block_90(block):
 #kiểm tra rồi
 def kiem_tra_khong_trung_screen(main_screen,next_screen):
     for colum in range(len(next_screen[0])):
-        if next_screen[len(next_screen)-1][colum]=="$":
+        if next_screen[len(next_screen)-1][colum]=="*":
             return False
     linh_canh=0
     for row in range(len(main_screen)):
         for colum in range(len(main_screen[row])):
-            if next_screen[row][colum]=="$" :
-                if main_screen[row][colum]=="$":
+            if next_screen[row][colum]=="*" :
+                if main_screen[row][colum]=="*":
                     linh_canh=1
     if linh_canh==1:
         return False
@@ -184,8 +189,8 @@ def xoa_dich_dong_i(main_screen,row_i):
         main_screen[row_i][colum]=" "
     for row in range(row_i,-1,-1):
         for colum in range(len(main_screen[row])):
-            if main_screen[row][colum]=="$":
-                main_screen[row+1][colum]="$"
+            if main_screen[row][colum]=="*":
+                main_screen[row+1][colum]="*"
                 main_screen[row][colum]=" "
     return main_screen
 
@@ -206,56 +211,105 @@ def kiem_tra_hang_day(main_screen,score):
 
 
 
+
 # kiểm tra gameover
 #input: main screen
 #output: True or False
 def check_gameover(main_screen,block):
     for colum in range(3,3+len(block[0]),1):
-        if main_screen[0][colum]=="$":
+        if main_screen[0][colum]=="*":
             return True
     return False
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 
-def input_keyboard(main_screen,block,range_of_block,score):
-    while True:
-        screen_phu=copy.deepcopy(screen)
 
-        left="a" #block dịch qua trái
-        right="d" #block dịch qua phải
-        xoay="w"   #block xoay 90 độ cùng chiều kim đồng hồ
-        down_all=" " # dấu cách -> block rơi xuống cùng
-        player_move=input() #phải có tác động từ bàn phím thì chương trình mới tiếp tục
-        if player_move==left:
-            if range_of_block != move_left(screen_phu,block,range_of_block) or kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,block,move_left(screen_phu,block,range_of_block))) :
+def compare_to_move_left(main_screen,block,range_of_block,score,player_move):
+    if player_move=="a":
+        if range_of_block != move_left(screen_phu,block,range_of_block): 
+            if kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,block,move_left(screen_phu,block,range_of_block))) :
                 range_of_block=move_left(screen_phu,block,range_of_block)
                 dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
-                time.sleep(0.1)    
+                time.sleep(1)
                 display_screen(dis_screen,score)
-        if player_move==right:
-            if range_of_block != move_right(screen_phu,block,range_of_block) or kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,block,move_right(screen_phu,block,range_of_block))) :
+    return range_of_block
+
+def compare_to_move_right(main_screen,block,range_of_block,score,player_move):
+    if player_move=="d":
+        if range_of_block != move_right(screen_phu,block,range_of_block):  
+            if kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,block,move_right(screen_phu,block,range_of_block))) :
                 range_of_block=move_right(screen_phu,block,range_of_block)
-                dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
-                time.sleep(0.1)
+                dis_screen=merge_block_with_screen(main_screen,block,range_of_block)                
+                time.sleep(1)
                 display_screen(dis_screen,score)
-        if player_move==xoay:
-            if  kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,xoay_block_90(block),range_of_block)) :
-                block=xoay_block_90(block)
-                dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
-                time.sleep(0.1)
-                display_screen(dis_screen,score)
-        if player_move==" ":
-            while True:
-                if range_of_block == down_1_line(screen_phu,block,range_of_block) or not kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,block,down_1_line(screen_phu,block,range_of_block))) :
-                    break      
-                range_of_block=down_1_line(screen_phu,block,range_of_block)
+    return range_of_block
+
+def  compare_to_move_xoay(main_screen,block,range_of_block,score,player_move):
+    if player_move=="w":
+        if  kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,xoay_block_90(block),range_of_block)) :
+            block=xoay_block_90(block)
             dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
+            time.sleep(1)                
             display_screen(dis_screen,score)
+    return block
+
+def  compare_to_drop(main_screen,block,range_of_block,score,player_move):
+    if player_move==" ":
+        while True:
+            if range_of_block == down_1_line(screen_phu,block,range_of_block) or not kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,block,down_1_line(screen_phu,block,range_of_block))) :
+                break      
+            range_of_block=down_1_line(screen_phu,block,range_of_block)
+        dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
+        display_screen(dis_screen,score)
+    return range_of_block
 
 ######test_không có tác động từ bàn phím#####
+
+
+def read_character():
+    while True:   
+        key=readchar.readchar()        
+        input_queue.put(key)
+        if key=="x":
+            break
+def thread_println(str):
+    print('\r'+str)
+
+def loop_down_1_line(main_screen,block,range_of_block,score):
+    while True:
+        if not input_queue.empty():    
+            player_move= input_queue.get()#phải có tác động từ bàn phím thì chương trình mới tiếp tục
+            range_of_block=compare_to_move_left(main_screen,block,range_of_block,score,player_move)
+            range_of_block=compare_to_move_right(main_screen,block,range_of_block,score,player_move)
+            block=compare_to_move_xoay(main_screen,block,range_of_block,score,player_move)
+            range_of_block=compare_to_drop(main_screen,block,range_of_block,score,player_move)        
+        if range_of_block == down_1_line(screen_phu,block,range_of_block) or not kiem_tra_khong_trung_screen(main_screen,merge_block_with_screen(screen_phu,block,down_1_line(screen_phu,block,range_of_block))) :
+            break  
+        range_of_block=down_1_line(main_screen,block,range_of_block)
+        dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
+        display_screen(dis_screen,score)
+    return range_of_block,block
+
+
+def input_keyboard(main_screen,block,range_of_block,score,input_queue):
+    while True:
+        block=get_new_block(blocks,range_of_block)
+        screen_phu=copy.deepcopy(screen)
+        new_scr=merge_block_with_screen(screen_phu,block,range_of_block)
+        dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
+        range_of_block,block=loop_down_1_line(main_screen,block,range_of_block,score)
+        main_screen=merge_block_with_screen(main_screen,block,range_of_block)
+        kiem_tra_hang_day(main_screen,score)
+        display_screen(main_screen,score)        
+        if check_gameover(main_screen,block):
+            break
+    thread_println("gameover")
+
+################3333
 main_screen=copy.deepcopy(screen)
 screen_phu=copy.deepcopy(screen)
 block=get_new_block(blocks,range_of_block)
-new_scr=merge_block_with_screen(screen_phu,block,range_of_block)
-dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
-display_screen(dis_screen,score)
-input_keyboard(main_screen,block,range_of_block,score)
+########
+input_queue=queue.Queue()
+x2 = threading.Thread(target=input_keyboard, args=(main_screen,block,range_of_block,score,input_queue))
+x3=threading.Thread(target=read_character)
+x2.start()
+x3.start()
