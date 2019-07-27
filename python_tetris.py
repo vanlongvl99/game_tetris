b53c8b56d6301a97fd3912accb635655000c6e06
vanlongvl99 <nguyenvanlongt2@gmail.com>
2019-07-27 10:00:40 +0700

    python_tetris.py

--- python_tetris.py
+++ python_tetris.py
@@ -9,54 +9,54 @@ import queue
 import datetime
 
 
-screen=[ [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "],
-         [" "," "," "," "," "," "," "," "," "," "]]
+screen=[ [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",],
+         [" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ",]]
 
-block_0=[["*","*"],
-         ["*","*"]]
+block_0=[["$","$"],
+         ["$","$"]]
           
-block_1=[["*","*","*"],
-         [" ","*"," "],
+block_1=[["$","$","$"],
+         [" ","$"," "],
          [" "," "," "]]
         
-block_2=[[" ","*","*"],
-         ["*","*"," "],
+block_2=[[" ","$","$"],
+         ["$","$"," "],
          [" "," "," "]]
 
-block_3=[["*"," "," "],
-         ["*","*","*"],
+block_3=[["$"," "," "],
+         ["$","$","$"],
          [" "," "," "]]
 
 block_4=[[" "," "," "," "],
-         ["*","*","*","*"],
+         ["$","$","$","$"],
          [" "," "," "," "],
          [" "," "," "," "],]
 
 
-block_5=[[" "," ","*"],
-         ["*","*","*"],
+block_5=[[" "," ","$"],
+         ["$","$","$"],
          [" "," "," "]]
 
-block_6=[["*","*"," "],
-         [" ","*","*"],
+block_6=[["$","$"," "],
+         [" ","$","$"],
          [" "," "," "]]
 
 
@@ -82,8 +82,8 @@ def merge_block_with_screen(screen_phu,block_x,range_of_block):
     for row in range(len(block_x)):
         block_row=block_x[row]
         for colum in range(len(block_row)):
-            if block_row[colum]=="*":
-                block_in_screen[row+range_of_block[0]][colum+range_of_block[1]]="*"
+            if block_row[colum]=="$":
+                block_in_screen[row+range_of_block[0]][colum+range_of_block[1]]="$"
     return block_in_screen
 
 #merge screen and display
@@ -102,7 +102,8 @@ def display_screen(screen_,score):
             string_row+=screen_[row][colum]
         merge_scr.append(string_row) 
     for row in range(len(merge_scr)):
-        thread_println(merge_scr[row])
+        thread_println("|"+merge_scr[row]+"|")
+    thread_println("=======================")
 
 #dich trai:
 #input: screen and block, range of block
@@ -114,7 +115,7 @@ def move_left(screen_phu,block_x,range_of_block):
     range_new=copy.deepcopy(range_of_block)
     linh_canh=1
     for row in range(len(new_screen)):
-        if new_screen[row][0]=="*":
+        if new_screen[row][0]=="$":
             return range_new
     range_new[1]=range_new[1]-1
     return range_new
@@ -129,7 +130,7 @@ def move_right(screen_phu,block_x,range_of_block):
     range_new=copy.deepcopy(range_of_block)
     linh_canh=1
     for row in range(len(new_screen)):
-        if new_screen[row][9]=="*":
+        if new_screen[row][len(new_screen[row])-1]=="$":
             return range_new
     range_new[1]+=1
     return range_new
@@ -142,12 +143,12 @@ def down_1_line(screen_phu,block,range_of_block):
     range_new=copy.deepcopy(range_of_block)
     for row in range(len(block)):
         for colum in range(len(block[row])):
-            if block[row][colum]=="*":
+            if block[row][colum]=="$":
                 last_row=row
-    if range_of_block[0]<len(screen_phu)-last_row-1 :
+    if range_of_block[0]<len(screen_phu)-last_row  :
         range_new[0]+=1
         return range_new
-    else:
+    else:  
         return range_new
  
 #xoay 90 độ
@@ -175,13 +176,13 @@ def xoay_block_90(block,range_of_block):
 #kiểm tra rồi
 def kiem_tra_khong_trung_screen(main_screen,next_screen):
     for colum in range(len(next_screen[0])):
-        if next_screen[len(next_screen)-1][colum]=="*":
+        if next_screen[len(next_screen)-1][colum]=="$":
             return False
     linh_canh=0
     for row in range(len(main_screen)):
         for colum in range(len(main_screen[row])):
-            if next_screen[row][colum]=="*" :
-                if main_screen[row][colum]=="*":
+            if next_screen[row][colum]=="$" :
+                if main_screen[row][colum]=="$":
                     linh_canh=1
     if linh_canh==1:
         return False
@@ -197,8 +198,8 @@ def xoa_dich_dong_i(main_screen,row_i):
         main_screen[row_i][colum]=" "
     for row in range(row_i,-1,-1):
         for colum in range(len(main_screen[row])):
-            if main_screen[row][colum]=="*":
-                main_screen[row+1][colum]="*"
+            if main_screen[row][colum]=="$":
+                main_screen[row+1][colum]="$"
                 main_screen[row][colum]=" "
     return main_screen
 
@@ -225,7 +226,7 @@ def kiem_tra_hang_day(main_screen,score):
 #output: True or False
 def check_gameover(main_screen,block):
     for colum in range(3,3+len(block[0]),1):
-        if main_screen[0][colum]=="*":
+        if main_screen[0][colum]=="$":
             return True
     return False
 
@@ -239,6 +240,7 @@ def compare_to_move_left(main_screen,block,range_of_block,score,player_move):
                 time.sleep(1)
                 display_screen(dis_screen,score)
     return range_of_block
+
 
 def compare_to_move_right(main_screen,block,range_of_block,score,player_move):
     if player_move=="d":
@@ -286,8 +288,7 @@ def next_to_down_1_line(main_screen,block,range_of_block,score):
     dis_screen=merge_block_with_screen(main_screen,block,range_of_block)
     display_screen(dis_screen,score)
     return main_screen,range_of_block
-
-
+    
 def loop_down_1_line(main_screen,block,range_of_block,score):
     pre_sec=datetime.datetime.now().second
     while True:
@@ -307,6 +308,7 @@ def loop_down_1_line(main_screen,block,range_of_block,score):
 
 def input_keyboard(main_screen,block,range_of_block,score,input_queue):
     while True:
+        input_queue.queue.clear()
         block=get_new_block(blocks,range_of_block)
         screen_phu=copy.deepcopy(screen)
         new_scr=merge_block_with_screen(screen_phu,block,range_of_block)
@@ -325,7 +327,7 @@ screen_phu=copy.deepcopy(screen)
 block=get_new_block(blocks,range_of_block)
 ########
 input_queue=queue.Queue()
-x2 = threading.Thread(target=input_keyboard, args=(main_screen,block,range_of_block,score,input_queue))
-x3=threading.Thread(target=read_character)
-x2.start()
-x3.start()
+loop_input = threading.Thread(target=input_keyboard, args=(main_screen,block,range_of_block,score,input_queue))
+loop_read_character=threading.Thread(target=read_character)
+loop_input.start()
+loop_read_character.start()
